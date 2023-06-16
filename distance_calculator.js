b126638d0ad03abb5478e5972257c6a2f3fb4f67
chinweugwu <ugwu.millicent@gmail.com>
2023-06-16 21:44:43 +0100

    Task 1 imperfect but complete

--- distance_calculator.js
+++ distance_calculator.js
@@ -1,94 +1,72 @@
+//Get required elements from html using their id's
 let pointA = document.getElementById("point_a_input");
 let pointB = document.getElementById("point_b_input");
 let map_display = document.getElementById("map_display")
 let distance = document.getElementById("distance_display");
 let distanceBtn = document.getElementById("distance_btn");
      
-
+    //function to implement haversian formula
      function haversine(point_A, point_B) {
-        let arr1 = point_A.split(",");
-        let arr2 = point_B.split(",");
+        if (point_A === "" || point_B === "") {
+            alert("You did not enter the correct coordinates");
+        } else {
+            console.log(pointA.value[0])
+            let arr1 = point_A.split(",");
+            let arr2 = point_B.split(",");
 
-        let lat1 = parseFloat(arr1[0]);
-        let lon1 = parseFloat(arr1[1]);
-        let lat2 = parseFloat(arr2[0]);
-        let lon2 = parseFloat(arr2[1]);
+            let lat1 = parseFloat(arr1[0]);
+            let lon1 = parseFloat(arr1[1]);
+            let lat2 = parseFloat(arr2[0]);
+            let lon2 = parseFloat(arr2[1]);
 
-        //distance between latitudes and longitudes
-        let dLat = (lat2 - lat1) * Math.PI / 180.0;
-        let dLon = (lon2 - lon1) * Math.PI / 180.0;
-           
-        // convert to radiansa
-        lat1 = (lat1) * Math.PI / 180.0;
-        lat2 = (lat2) * Math.PI / 180.0;
-         
-        // apply formulae
-        let a = Math.pow(Math.sin(dLat / 2), 2) +
-                   Math.pow(Math.sin(dLon / 2), 2) *
-                   Math.cos(lat1) *
-                   Math.cos(lat2);
-        let rad = 6371;
-        let c = 2 * Math.asin(Math.sqrt(a));
-        return rad * c;     
+            //distance between latitudes and longitudes
+            let dLat = (lat2 - lat1) * Math.PI / 180.0;
+            let dLon = (lon2 - lon1) * Math.PI / 180.0;
+            
+            // convert to radiansa
+            lat1 = (lat1) * Math.PI / 180.0;
+            lat2 = (lat2) * Math.PI / 180.0;
+            
+            // apply formulae
+            let a = Math.pow(Math.sin(dLat / 2), 2) +
+                    Math.pow(Math.sin(dLon / 2), 2) *
+                    Math.cos(lat1) *
+                    Math.cos(lat2);
+            let rad = 6371;
+            let c = 2 * Math.asin(Math.sqrt(a));
+            return rad * c;     
+        } 
      }
 
     distanceBtn.addEventListener('click', () => {
-      console.log(haversine(pointA.value, pointB.value)+'km');
-      let final = Math.round(haversine(pointA.value, pointB.value));
-      let finalDistance = final.toFixed(2)
-      distance.innerHTML = `<h2>Distance: ${finalDistance +'km'}</h2>`  
+            let final = Math.round(haversine(pointA.value, pointB.value));
+            let finalDistance = final.toFixed(2)
+            console.log(typeof(pointA.value), typeof(pointB.value));
+            distance.innerHTML = `<h2>Distance: ${finalDistance +'km'}</h2>`
     });
 
+const key = '1kxCdzfMb0XZnYe0LRS6';
 
+const map = L.map('map_display').setView([0, 0], 1);
+const mtLayer = L.maptilerLayer({
+    apiKey: key,
+    style: L.MaptilerStyle.STREETS, // optional
+    geolocate: true //set the initial view to the user's location
+}).addTo(map);
 
+L.control.maptilerGeocoding({ apiKey: key }).addTo(map);
 
-const map = new ol.Map({
-  layers: [
-      new ol.layer.Tile({
-          source: new ol.source.TileJSON({
-              url: 'https://api.maptiler.com/maps/basic-v2/tiles.json?key=1kxCdzfMb0XZnYe0LRS6',
-              tileSize: 512,
-          })
-      })
-  ],
-  target: 'map_display',
-  view: new ol.View({
-      center:ol.proj.fromLonLat([9.0778, 8.6775]),
-      zoom: 5
-  })
-});
-
-const marker = new ol.layer.Vector({
-  source: new ol.source.Vector({
-      features: [
-          new ol.Feature({
-              geometry: new ol.geom.Point(
-                  ol.proj.fromLonLat([9.0778, 8.6775])
-              )
-          })
-      ],
-  }),
-  style: new ol.style.Style({
-      image: new ol.style.Icon({
-          crossOrigin: 'anonymous',
-          anchor: [0.5, 1],
-          src: 'https://docs.maptiler.com/openlayers/default-marker/marker-icon.png',
-      })
-  })
-})
-const Airports = new ol.layer.Vector({
-                          source: new ol.source.Vector({
-                              url: `https://api.maptiler.com/data/f0f125a3-50ed-477a-8055-56e4d9a51ab2/features.json?key=1kxCdzfMb0XZnYe0LRS6`,
-                                  format: new ol.format.GeoJSON(),
-                          }),
-                          style:  new ol.style.Style({
-                                  image: new ol.style.Icon({
-                                          src: 'https://docs.maptiler.com/openlayers/geojson-points/icon-plane-512.png',
-                                          size: [512, 512],
-                                          scale: 0.03
-                                  })
-                          }) 
-                      })
-
-map.addLayer(marker);
-map.addLayer(Airports);
+//make marker global scope
+let marker1;
+//function to fire when user clicks on a map
+function onMapClick(e) {
+    //check for existence of a marker from previous click, if there is a marker, then it would be removed
+    if (marker1) {
+        map.removeLayer(marker1);
+    }
+    //assign marker to variable
+    marker1 = L.marker(e.latlng).addTo(map);
+    //bind pop up to marker
+    marker1.bindPopup(`${e.latlng}`).openPopup()
+}
+map.on('click', onMapClick);
